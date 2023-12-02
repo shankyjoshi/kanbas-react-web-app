f057ec56a2735b1d497484f1c312175fbf625dda
Shashank Joshi <shashankjoshi368@gmail.com>
2023-12-02 18:50:10 -0500

    commit

--- src/Project/users/account.js
+++ src/Project/users/account.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 function Account() {
   const [account, setAccount] = useState(null);
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
   const fetchUserById = async (id) => {
     const user = await client.findUserById(id);
@@ -13,7 +14,15 @@ function Account() {
     setAccount(account);
   };
   const save = async () => {
-    await client.updateUser(account);
+    try {
+      await client.updateUser(account);
+      setError({ message: "Update was successful!", type: "success" });
+    } catch (err) {
+      setError({
+        message: "Update was not successful. Please try again!",
+        type: "danger",
+      });
+    }
   };
   const { id } = useParams();
   useEffect(() => {
@@ -27,15 +36,25 @@ function Account() {
     await client.signout();
     navigate("/project/signin");
   };
-
+  const formatUtcDate = (utcTimestamp) => {
+    const utcDate = new Date(utcTimestamp);
+    const year = utcDate.getUTCFullYear();
+    const month = String(utcDate.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(utcDate.getUTCDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
   return (
     <div className="w-50">
       <h1>Account</h1>
+      {error && (
+        <div className={`alert alert-${error.type}`}>{error.message}</div>
+      )}
       {account && (
         <div className="d-flex flex-column col-6 gap-2">
           <input
             type="text"
             value={account.username}
+            placeholder="username"
             className="form-control"
             disabled
           />
@@ -43,6 +62,7 @@ function Account() {
           <input
             type="password"
             value={account.password}
+            placeholder="password"
             className="form-control"
             onChange={(e) =>
               setAccount({ ...account, password: e.target.value })
@@ -51,30 +71,39 @@ function Account() {
           <input
             value={account.firstName}
             className="form-control"
+            placeholder="firstName"
             onChange={(e) =>
               setAccount({ ...account, firstName: e.target.value })
             }
           />
           <input
             value={account.lastName}
+            placeholder="lastName"
             className="form-control"
             onChange={(e) =>
               setAccount({ ...account, lastName: e.target.value })
             }
           />
+
           <input
+            type="date"
             className="form-control"
-            value={account.dob}
+            id="exampleInputDate"
+            placeholder="Select a Date"
+            value={formatUtcDate(account.dob)}
             onChange={(e) => setAccount({ ...account, dob: e.target.value })}
           />
+
           <input
             className="form-control"
             value={account.email}
+            placeholder="email"
             onChange={(e) => setAccount({ ...account, email: e.target.value })}
           />
           <select
             className="form-control"
             value={account.role}
+            placeholder="role"
             onChange={(e) => setAccount({ ...account, role: e.target.value })}
           >
             <option value="USER">User</option>
@@ -82,12 +111,16 @@ function Account() {
             <option value="FACULTY">Faculty</option>
             <option value="STUDENT">Student</option>
           </select>
-          <button className="btn btn-primary " onClick={save}>
-            Save
-          </button>
-          <button className="btn btn-danger " onClick={signout}>
-            signout
-          </button>
+          {!id && (
+            <>
+              <button className="btn btn-primary " onClick={save}>
+                Save
+              </button>
+              <button className="btn btn-danger " onClick={signout}>
+                signout
+              </button>
+            </>
+          )}
           <Link to="/project/admin/users" className="btn btn-warning w-100">
             Users
           </Link>
--- src/Project/users/signin.js
+++ src/Project/users/signin.js
@@ -1,38 +1,57 @@
 import * as client from "./client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Signin() {
   const [credentials, setCredentials] = useState({
     username: "",
     password: "",
   });
+  const [error, setError] = useState(null);
+  const [account, setAccount] = useState(null);
   const navigate = useNavigate();
   const signin = async () => {
-    await client.signin(credentials);
-    navigate("/project/account");
+    try {
+      await client.signin(credentials);
+      navigate("/project/account");
+    } catch (err) {
+      setError({ message: "Invalid credentials" });
+    }
   };
+  const fetchAccount = async () => {
+    const account = await client.account();
+    setAccount(account);
+  };
+  useEffect(() => {
+    fetchAccount();
+  }, []);
   return (
-    <div className="d-flex flex-column col-2 gap-2">
-      <h1>Signin</h1>
-      <input
-        className="form-control"
-        value={credentials.username}
-        onChange={(e) =>
-          setCredentials({ ...credentials, username: e.target.value })
-        }
-      />
-      <input
-        className="form-control"
-        value={credentials.password}
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
-      />
-      <button className="btn btn-success " onClick={signin}>
-        {" "}
-        Signin{" "}
-      </button>
-    </div>
+    <>
+      {account ? (
+        <h1> You are already Signed In as {account.username}! </h1>
+      ) : (
+        <div className="d-flex flex-column col-2 gap-2">
+          <h1>Signin</h1>
+          {error && <div className="alert alert-danger">{error.message}</div>}
+          <input
+            className="form-control"
+            value={credentials.username}
+            onChange={(e) =>
+              setCredentials({ ...credentials, username: e.target.value })
+            }
+          />
+          <input
+            className="form-control"
+            value={credentials.password}
+            onChange={(e) =>
+              setCredentials({ ...credentials, password: e.target.value })
+            }
+          />
+          <button className="btn btn-success " onClick={signin}>
+            Signin
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 export default Signin;
